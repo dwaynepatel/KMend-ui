c2bf76542ebe98d9b7869dd06dcb8ceb39c6d88f
Dwayne Patel <dwaynepatel@hotmail.com>
2021-04-26 19:35:07 +0100

    added bar chart, filtrering and material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { UserService } from './user.service';
 
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import { MatSliderChange } from '@angular/material/slider';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,17 @@ import { Color, Label } from 'ng2-charts';
 })
 export class AppComponent {
   title = 'tech-test-ui';
-
+  spend: any[] = [];
   userList: any[] = [];
+  MyChartData: any[] = [];
+  regionList: any[] = [];
 
 
    lineChartData: ChartDataSets[] = [
-    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
-    { data: [185, 172, 178, 175, 177, 175], label: 'Crude oil prices 100' },
-  ];
+    ];
 
-  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                              'August', 'September', 'October', 'November', 'December'];
 
   lineChartOptions = {
     responsive: true,
@@ -38,7 +40,9 @@ export class AppComponent {
   lineChartType = 'line';
 
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {
+  
+  }
 
   ngOnInit() {
 
@@ -46,13 +50,21 @@ export class AppComponent {
     this.userService.getUsers();
 
     this.userService.getUsers().subscribe(result => {
-     const userList = result;
-      // console.log(this.userList);
+     this.userList = result;
+    //  populate spend list 
+     this.spend = this.userList.map(user => user.spend);
+     // populate region list with unique values only
+     this.regionList =this.userList.map(user => user.region).filter((value, index, self) => self.indexOf(value) === index);
+  
+    console.log(this.regionList);
 
 //filter on region
 // this.userList = userList.filter(0 -> o.region ==='United States');
-this.userList = userList.filter(region => region.region ==='United States');
+// this.userList = userList.filter(region => region.region ==='United States');
+
 console.log(this.userList);
+console.log("spend: " + this.spend);
+this.updateChart();
     },
     err => {
       console.error('error loading', err);
@@ -61,5 +73,14 @@ console.log(this.userList);
 
 
   } 
+  onInputChange(event: MatSliderChange) {
+    console.log("This is emitted as the thumb slides");
+    console.log(event.value);
+  }
+
+  updateChart() {
+    this.lineChartData = 
+   [ { data: this.spend, label: 'Crude oil prices' }]
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserService } from './user.service';
 import { ChartsModule } from 'ng2-charts';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -11,9 +14,12 @@ import { ChartsModule } from 'ng2-charts';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    ChartsModule
+    ChartsModule,
+    MatSliderModule,
+    MatSelectModule
     
   ],
   providers: [UserService],
