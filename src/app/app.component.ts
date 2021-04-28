fa33e00e508f456ef208c9cb3c6f4c431f490aac
Dwayne Patel <dwaynepatel@hotmail.com>
2021-04-28 14:34:27 +0100

    added line charts and table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { UserService } from './user.service';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 import { MatSliderChange } from '@angular/material/slider';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,17 @@ import { MatSliderChange } from '@angular/material/slider';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tech-test-ui';
-  spend: any[] = [];
+
   userList: any[] = [];
+  userListfiltered: any[] = [];
   MyChartData: any[] = [];
   regionList: any[] = [];
+  monthlySpend: any[] = [];
+  monthlyList: any[] = [];
+  cumulativeList: any[] = [];
+
+  
+  
 
 
    lineChartData: ChartDataSets[] = [
@@ -38,6 +45,7 @@ export class AppComponent {
   lineChartLegend = true;
   lineChartPlugins = [];
   lineChartType = 'line';
+  avg: unknown;
 
 
   constructor(private userService: UserService) {
@@ -46,24 +54,16 @@ export class AppComponent {
 
   ngOnInit() {
 
-    
-    this.userService.getUsers();
-
+    // retrieve list of users from the service
     this.userService.getUsers().subscribe(result => {
      this.userList = result;
-    //  populate spend list 
-     this.spend = this.userList.map(user => user.spend);
+  
      // populate region list with unique values only
      this.regionList =this.userList.map(user => user.region).filter((value, index, self) => self.indexOf(value) === index);
   
     console.log(this.regionList);
 
-//filter on region
-// this.userList = userList.filter(0 -> o.region ==='United States');
-// this.userList = userList.filter(region => region.region ==='United States');
 
-console.log(this.userList);
-console.log("spend: " + this.spend);
 this.updateChart();
     },
     err => {
@@ -76,11 +76,44 @@ this.updateChart();
   onInputChange(event: MatSliderChange) {
     console.log("This is emitted as the thumb slides");
     console.log(event.value);
+
+    this.getMonthlyUsers(event.value);
+    
   }
 
   updateChart() {
-    this.lineChartData = 
-   [ { data: this.spend, label: 'Crude oil prices' }]
+     this.lineChartData = 
+   [ { data: this.monthlyList, label: 'Monthly' },
+   { data: this.cumulativeList, label: 'Cumulative' } ] 
+   
+  
   }
+  getMonthlyUsers(filterBySlider? : number) {
+    this.monthlyList = [];
+    for (let i = 1; i < 13; i++) {
+      if (filterBySlider) {
+        this.monthlyList.push(this.userList.filter(user => user.birthday ==  i && user.spend > filterBySlider).length);
+      } else {
+    this.monthlyList.push(this.userList.filter(user => user.birthday == i).length);
+      }
+
+  }// end loop
+  console.log("average out of  loop: " + (this.monthlyList));
+  this.getCumulativeUsers();
+} 
+
+getCumulativeUsers(filterBySlider? : number) {
+  this.cumulativeList = [];
+ this.cumulativeList = this.monthlyList.map(element => element * 5)
+          .map((content, index, array) =>
+          {
+            
+            return array[index] += array[index-1] ? array[index-1] : 0
+          })
+
+
+this.updateChart();
+console.log("cumulativeList list" + this.cumulativeList);
+} 
 
 }
